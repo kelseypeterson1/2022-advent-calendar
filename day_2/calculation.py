40daf4d9eecea51796143d7bdc18ff70b2061ede
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-12-02 21:42:32 -0600

    day 2 - second star collected

--- day_2/calculation.py
+++ day_2/calculation.py
@@ -18,9 +18,35 @@ def calculate_choice_points(choice):
             return 2
         case "Z":
             return 3
-        
+
 points = 0
 for round in input_data:
     points += calculate_winner_points(round[0], round[2]) + calculate_choice_points(round[2])
 
 print(points)
+    
+def calculate_choice_points_v2(opp, result):
+    if opp == "A" and result == "Y" or opp == "B" and result == "X" or opp == "C" and result == "Z":
+        return 1
+        #rock
+    elif opp == "A" and result == "Z" or opp == "B" and result == "Y" or opp == "C" and result == "X":
+        return 2
+        #paper
+    else:
+        return 3
+        #scissors
+
+def calculate_winner_points_v2(choice):
+    match choice:
+        case "X":
+            return 0
+        case "Y":
+            return 3
+        case "Z":
+            return 6
+
+points_v2 = 0
+for round in input_data:
+    points_v2 += calculate_winner_points_v2(round[2]) + calculate_choice_points_v2(round[0], round[2])
+
+print(points_v2)
