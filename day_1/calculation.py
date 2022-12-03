8e8b52dd7a961cddeab3acd33adf6703bba41256
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-12-02 20:45:36 -0600

    day 1 - first star collected

--- day_1/calculation.py
+++ day_1/calculation.py
@@ -1,3 +1,15 @@
 with open('input_file.txt') as f:
-    input = f.readlines()
-print(input)
+    text = f.readlines()
+    elves = []
+    elf = []
+    for piece in text:
+        if piece != "\n":
+            calories = piece.replace("\n","")
+            elf.append(int(calories))
+        else:
+            elves.append(elf)
+            elf = []
+
+max_calories = max(list(map(lambda elf: sum(elf), elves)))
+
+print(max_calories)
